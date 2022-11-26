9f63678bcc66117a80a35611ab5a8931d52d60c8
Matthias Kalms <mtkalms@gmail.com>
2022-11-25 23:17:39 -0800

    Add ridgeline

---
+++ graphical/ridgeline.py
@@ -0,0 +1,104 @@
+from dataclasses import dataclass
+from typing import List, Union, Optional, Tuple
+
+from rich.box import Box, HEAVY
+from rich.color import Color
+from rich.console import Console, ConsoleOptions, RenderResult
+from rich.measure import Measurement
+from rich.segment import Segment
+from rich.style import Style
+
+from graphical.sparkline import SparklineStyle, SparklineRenderer
+
+
+@dataclass
+class RidgelineRow:
+    label: str
+    values: List[float]
+    color: Union[Color, str] = "default"
+    graph_style: Optional[SparklineStyle] = None
+
+
+class RidgelineGraph:
+
+    def __init__(
+        self,
+        title: str,
+        value_range: Optional[Tuple[int, int]] = None,
+        color: Union[Color, str] = "default",
+        graph_style: SparklineStyle = SparklineStyle.AREA,
+        box: Optional[Box] = HEAVY
+    ):
+        self.title = title
+        self.value_range = value_range
+        self.style = Style(color=color)
+        self.graph_style = graph_style
+        self.box = box
+        self.rows: List[RidgelineRow] = []
+
+    def add_row(
+        self,
+        label: str,
+        values: List[float],
+        color: Union[Color, str] = "default",
+        graph_style: Optional[SparklineStyle] = None
+    ) -> RidgelineRow:
+        row = RidgelineRow(label, values, color, graph_style)
+        self.rows.append(row)
+        return row
+
+    def __rich_console__(self, console: Console, options: ConsoleOptions) -> RenderResult:
+        width_labels = max(len(d.label) for d in self.rows) + 1
+        width_graphs = max(len(d.values) for d in self.rows)
+
+        yield Segment(" " * width_labels)
+        yield Segment(f"{self.title : ^{width_graphs + 2}}")
+        yield Segment("\n")
+
+        yield Segment(" " * width_labels)
+        yield Segment(self.box.top_left)
+        yield Segment(self.box.top * width_graphs)
+        yield Segment(self.box.top_right)
+        yield Segment("\n")
+
+        for row in self.rows:
+            style = Style(color=row.color) if row.color != "default" else self.style
+
+            yield Segment(f"{row.label : >{width_labels - 1 }} ")
+            yield Segment(self.box.row_right)
+
+            graph_style = row.graph_style if row.graph_style else self.graph_style
+            graph = SparklineRenderer.render(
+                row.values,
+                value_range=self.value_range,
+                graph_style=graph_style
+            )
+            yield Segment(graph, style)
+            yield Segment(self.box.mid_right)
+            yield Segment("\n")
+
+        yield Segment(" " * width_labels)
+        yield Segment(self.box.bottom_left)
+        yield Segment(self.box.bottom * width_graphs)
+        yield Segment(self.box.bottom_right)
+        yield Segment("\n")
+
+    def __rich_measure__(self, console: Console, options: ConsoleOptions) -> Measurement:
+        pass
+
+
+if __name__ == '__main__':
+    from rich import print
+    from random import randint
+    from math import sin, pi
+
+    def wave(r: int) -> int:
+        return int(sin((r + randint(0, 3)) * 2 * pi / 25) * 40 - 50)
+
+    graph = RidgelineGraph("Ridgeline Graph Example", color="purple")
+    for b in range(12):
+        data = [wave(d) for d in range(100)]
+        graph.add_row(f"row {b}", data)
+
+    print()
+    print(graph)
