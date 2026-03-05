0a37cbf864b4b0973a1ee601bee28508f6565bed
Matthias Kalms <mtkalms@gmail.com>
2026-03-05 07:13:47 -0800

    Add stretch option to buckets

--- src/graphical/_buckets.py
+++ src/graphical/_buckets.py
@@ -1,13 +1,20 @@
-from typing import Callable, Generator, Sequence, Union
+from typing import Callable, Iterable, Sequence, Union
 
 Numeric = Union[int, float]
 SummaryFunction = Callable[[Sequence[Numeric]], float]
 
 
 def buckets(
-    values: Sequence[Numeric], count: int, summary_function: SummaryFunction
-) -> Generator[float]:
+    values: Sequence[Numeric],
+    count: int,
+    summary_function: SummaryFunction,
+    stretch: bool = True,
+) -> Iterable[Sequence[Numeric]]:
     step = len(values) / count
     for d in range(count):
-        segment = values[int(d * step) : int((d + 1) * step)]
+        lower = int(d * step)
+        upper = int((d + 1) * step)
+        if stretch:
+            upper = max(upper, lower + 1)
+        segment = values[lower:upper]
         yield summary_function(segment)
