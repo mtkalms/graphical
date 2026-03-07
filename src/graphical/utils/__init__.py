eedf377e9a88b7094420a26de53ca881ce060a69
Matthias Kalms <mtkalms@gmail.com>
2026-03-07 12:33:25 -0800

    Also expose SummaryFunction and Numeric

--- src/graphical/utils/__init__.py
+++ src/graphical/utils/__init__.py
@@ -1,5 +1,5 @@
-from ._buckets import buckets
+from ._buckets import buckets, Numeric, SummaryFunction
 from ._invert_style import invert_style
 from ._normalize import normalize
 
-__all__ = ["buckets", "invert_style", "normalize"]
+__all__ = ["buckets", "invert_style", "normalize", "SummaryFunction", "Numeric"]
