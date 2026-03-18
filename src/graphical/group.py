23645cce73d67b0e0512e47ffda4adcb2f82cc16
Matthias Kalms <mtkalms@gmail.com>
2026-03-18 10:18:29 -0700

    Docs: Add documentation to Horizontal

--- src/graphical/group.py
+++ src/graphical/group.py
@@ -1,7 +1,7 @@
 from itertools import zip_longest, chain
-from typing import Any, Iterable, List, Protocol, Union
+from typing import Any, Iterable, List, Protocol
 
-from rich.console import Console, ConsoleOptions, RenderResult, RenderableType
+from rich.console import Console, ConsoleOptions, RenderResult
 from rich.measure import Measurement
 from rich.segment import Segment
 
@@ -11,7 +11,7 @@ def _add_between(array: Iterable[Any], insert: Any) -> List[Any]:
     return [d for d in chain.from_iterable(gapped) if d]
 
 
-class GraphicalGroupRenderable(Protocol):
+class GroupRenderable(Protocol):
     def __graphical_group__(
         self, console: Console, options: ConsoleOptions
     ) -> RenderResult:  # pragma: no cover
@@ -19,7 +19,15 @@ class GraphicalGroupRenderable(Protocol):
 
 
 class Horizontal:
-    def __init__(self, *renderables: GraphicalGroupRenderable, gap: int = 0) -> None:
+    """Arranges renderables horizontally.
+    All renderables need to implement the `GroupRenderable` protocol.
+
+    Args:
+        *renderables (GroupRenderable): Renderables to be arranged horizontally.
+        gap (int, optional): Gap between renderables. Defaults to 0.
+    """
+
+    def __init__(self, *renderables: GroupRenderable, gap: int = 0) -> None:
         self._renderables = renderables
         self._gap = gap
 
@@ -28,17 +36,12 @@ class Horizontal:
     ) -> Measurement:
         return Measurement(options.max_width, options.max_width)
 
-    def render_columns(
-        self, console: Console, options: ConsoleOptions
-    ) -> List[List[Union[RenderableType, Segment]]]:
-        return [
-            list(d.__graphical_group__(console, options)) for d in self._renderables
-        ]
-
     def __rich_console__(
         self, console: Console, options: ConsoleOptions
     ) -> RenderResult:
-        rendered = self.render_columns(console, options)
+        rendered = [
+            list(d.__graphical_group__(console, options)) for d in self._renderables
+        ]
         for row in zip_longest(*rendered):
             if self._gap > 0:
                 row = _add_between(row, Segment(" " * self._gap))
