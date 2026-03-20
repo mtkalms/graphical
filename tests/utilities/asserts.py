8034536c59d3a0135f296a89cccca4571d8cbf2b
Matthias Kalms <mtkalms@gmail.com>
2026-03-20 11:05:01 -0700

    Test: Allow different preview styles

--- tests/utilities/asserts.py
+++ tests/utilities/asserts.py
@@ -1,16 +1,33 @@
 from itertools import zip_longest
-from typing import Optional
+import re
+from typing import Literal, Optional
 from tests.utilities.render import render_ansi
 
 from rich.console import RenderableType, JustifyMethod, OverflowMethod
 from rich.text import Text
+from rich.style import Style
+
+PreviewStyle = Literal["visual", "ansi", "markup"]
+
+
+def _text_to_markup(text: Text) -> str:
+    result = ""
+    for span in text._spans:
+        chars = text.plain[span.start : span.end]
+        if span.style and span.style != Style.null():
+            style = str(span.style)
+            style = re.sub(r"\s?not \w+\s?", " ", style)
+            result += f"[{style}]{chars}[/{style}]"
+        else:
+            result += chars
+    return result
 
 
 def assert_markup(
     renderable: RenderableType,
     markup: str,
     message: Optional[str] = None,
-    preview: Optional[bool] = False,
+    preview: Optional[PreviewStyle] = None,
     justify: Optional[JustifyMethod] = None,
     overflow: Optional[OverflowMethod] = None,
     width: int = 80,
@@ -21,20 +38,26 @@ def assert_markup(
     )
     rendered_b = render_ansi(Text.from_markup(markup, end=""), width=width)
     if preview:
-        _preview_ansi(rendered_a, rendered_b)
+        _preview_ansi(rendered_a, rendered_b, preview)
     assert rendered_a == rendered_b, message
 
 
-def _preview_ansi(a: str, b: str):
+def _preview_ansi(a: str, b: str, style: PreviewStyle):
+    def cell_repr(c: str) -> str:
+        if style == "ansi":
+            return repr(c)
+        if style == "markup":
+            return _text_to_markup(Text.from_ansi(c))
+        return c
+
     lines_a = a.splitlines(keepends=True)
     lines_b = b.splitlines(keepends=True)
     # Get max line length (without ANSI codes)
-    max_len = max(len(Text.from_ansi(a)) for a in lines_a)
     print()
     print("Preview:")
     print(
         "\n".join(
-            f"{repr(a)}".ljust(max_len + 2) + f"\t{repr(b)}"
+            f"  actual: {cell_repr(a)}\n  expect: {cell_repr(b)}"
             for a, b in zip_longest(lines_a, lines_b)
         )
     )
