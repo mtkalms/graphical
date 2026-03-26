e9c6d139defbc4fd9455123f3f282e216fe9bd93
Matthias Kalms <mtkalms@gmail.com>
2026-03-26 14:58:29 -0700

    Add b-spline interpolation

---
+++ src/graphical/scale/chromatic.py
@@ -0,0 +1,33 @@
+from math import floor
+
+
+def _spline(t: float, p0: float, p1: float, p2: float, p3: float) -> float:
+    """Calculates a cubic B-spline point for a given t [0, 1]."""
+    t = max(0.0, min(1.0, t))
+    b0 = ((1 - t) ** 3) / 6
+    b1 = (3 * t**3 - 6 * t**2 + 4) / 6
+    b2 = (-3 * t**3 + 3 * t**2 + 3 * t + 1) / 6
+    b3 = (t**3) / 6
+    return b0 * p0 + b1 * p1 + b2 * p2 + b3 * p3
+
+
+def interpolate(t: float, *values: float) -> float:
+    t = max(0.0, min(1.0, t))
+    n = len(values) - 1
+    i = floor(t * n)
+    p1 = values[i]
+    p2 = values[i + 1]
+    # Edge behaviour: Add additional value with similar spacing
+    p0 = values[i - 1] if i > 0 else 2 * p1 - p2
+    p3 = values[i + 2] if i < (n - 1) else 2 * p2 - p1
+    return _spline(t, p0, p1, p2, p3)
+
+
+def interpolate_closed(t: float, *values: float) -> float:
+    n = len(values) - 1
+    i = floor(t * n)
+    p0 = values[(i - 1) % n]
+    p1 = values[i % n]
+    p2 = values[(i + 1) % n]
+    p3 = values[(i + 2) % n]
+    return _spline(t, p0, p1, p2, p3)
