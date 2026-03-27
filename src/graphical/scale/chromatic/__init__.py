ad44099467cc089804a173d1b414063a06021a09
Matthias Kalms <mtkalms@gmail.com>
2026-03-26 19:14:05 -0700

    Make chromatic a proper module

--- src/graphical/scale/chromatic/__init__.py
+++ src/graphical/scale/chromatic/__init__.py
@@ -1,4 +1,6 @@
 from math import floor
+from typing import Optional
+from rich.color import Color
 
 
 def _spline(t: float, p0: float, p1: float, p2: float, p3: float) -> float:
@@ -11,7 +13,7 @@ def _spline(t: float, p0: float, p1: float, p2: float, p3: float) -> float:
     return b0 * p0 + b1 * p1 + b2 * p2 + b3 * p3
 
 
-def interpolate(t: float, *values: float) -> float:
+def _interpolate(t: float, *values: float) -> float:
     t = max(0.0, min(1.0, t))
     n = len(values) - 1
     i = floor(t * n)
@@ -23,7 +25,7 @@ def interpolate(t: float, *values: float) -> float:
     return _spline(t, p0, p1, p2, p3)
 
 
-def interpolate_closed(t: float, *values: float) -> float:
+def _interpolate_closed(t: float, *values: float) -> float:
     n = len(values) - 1
     i = floor(t * n)
     p0 = values[(i - 1) % n]
